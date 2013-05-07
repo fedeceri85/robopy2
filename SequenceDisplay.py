e2e2759aef56658e39c4058df54fdac2d9d44c54
Catalin Ciubotaru <catalin.ciubotaru@gmail.com>
2013-05-07 22:04:38 +0200

    first draft -- playing a sequence works

--- SequenceDisplay.py
+++ SequenceDisplay.py
@@ -16,11 +16,24 @@ class SequenceDisplay(Ui_SequenceDisplayWnd, PyQt4.QtGui.QMainWindow):
 		self.setupUi(self)
 		self.imageWidgetSetup()
 		
+		#l = dir(self.CurrentFrameSlider)
+		#for i in l:
+		#	print(i)
+		
+		#meths = self.CurrentFrameSlider.?
+		#print(meths)
+		#self.CurrentFrameSlider.setBuddy(self.CurrentFrameSpinBox)
+		
 		self.RoboMainWnd = parent
 		self.SequenceFiles = files
 		self.MaxFrames = 0
+		self.CurrentShownFrame = 0
 		self.FramesPerFile = list()
+		
 		self.Tiff = None
+		self.PlayInterframe = 10
+		self.IsPlaying = False
+		self.timer = QBasicTimer()
 		
 		self.makeConnections()
 		
@@ -34,12 +47,16 @@ class SequenceDisplay(Ui_SequenceDisplayWnd, PyQt4.QtGui.QMainWindow):
 		imWidget = ImageDisplayWidget()
 		imWidget.SequenceDisplay = self
 		
+		self.imWidget = imWidget
+		
 		hlay.addWidget(imWidget)
 		
 	def makeConnections(self):
-		pass
-		#self.connect(self.roboActionOpen, SIGNAL("triggered()"), self.roboActionOpenCb)
-		
+		self.connect(self.ForwardFrameButton, SIGNAL("clicked()"), self.getNextSequenceFrame)
+		self.connect(self.BackFrameButton, SIGNAL("clicked()"), self.getPreviousSequenceFrame)
+		self.connect(self.FirstFrameButton, SIGNAL("clicked()"), self.getFirstSequenceFrame)
+		self.connect(self.LastFrameButton, SIGNAL("clicked()"), self.getLastSequenceFrame)
+		self.connect(self.PlayButton, SIGNAL("clicked()"), self.playButtonCb)
 		
 	def showStatusMessage(self, msg):
 		self.statusBar().showMessage(msg)
@@ -51,8 +68,9 @@ class SequenceDisplay(Ui_SequenceDisplayWnd, PyQt4.QtGui.QMainWindow):
 				th = TiffSequence(s)
 				self.MaxFrames = self.MaxFrames + th.getFrames()
 				self.showStatusMessage("Counted " + str(self.MaxFrames) + " frames")
-				self.FramesPerFile.append(th.getFrames())
-			
+				self.FramesPerFile.append(th.getFrames())	
+				
+			self.updateFrameWidgetsRange()
 	
 	def getSequenceFrame(self, n):
 		(i,j) = self.getSequenceIndexes(n)
@@ -68,7 +86,65 @@ class SequenceDisplay(Ui_SequenceDisplayWnd, PyQt4.QtGui.QMainWindow):
 		if im == None:
 			return None
 		
+		self.showStatusMessage("Frame " + str(self.CurrentShownFrame+1) + " of " + str(self.MaxFrames))
+		
 		return SequenceProcessor.convert16Bitto8Bit(im, im.min(), im.max(), True)
+		
+	def getNextSequenceFrame(self):
+		self.CurrentShownFrame = self.CurrentShownFrame + 1
+		if self.CurrentShownFrame >= self.MaxFrames:
+			self.showStatusMessage("Trying to show frame " + str(self.CurrentShownFrame) + " of " + str(self.MaxFrames) + ". Impossible!!")
+			self.CurrentShownFrame = self.CurrentShownFrame - 1
+			return
+		
+			
+		self.FrameImage = self.getSequenceFrame(self.CurrentShownFrame)
+		if self.FrameImage == None:
+			print("getFrame returned None")
+		self.imWidget.repaint()
+		self.updateCurrentFrameWidgets()
+		
+	def getPreviousSequenceFrame(self):
+		self.CurrentShownFrame = self.CurrentShownFrame - 1
+		if self.CurrentShownFrame < 0:
+			self.showStatusMessage("Trying to show frame " + str(self.CurrentShownFrame) + " of " + str(self.MaxFrames) + ". Impossible!!")
+			self.CurrentShownFrame = self.CurrentShownFrame + 1
+			return
+		
+		
+		self.FrameImage = self.getSequenceFrame(self.CurrentShownFrame)
+		self.imWidget.repaint()
+		self.updateCurrentFrameWidgets()
+		
+	def getFirstSequenceFrame(self):
+		self.CurrentShownFrame = 0
+		self.FrameImage = self.getSequenceFrame(self.CurrentShownFrame)
+		self.imWidget.repaint()
+		self.updateCurrentFrameWidgets()
+		
+	def getLastSequenceFrame(self):
+		self.CurrentShownFrame = self.MaxFrames - 1
+		self.FrameImage = self.getSequenceFrame(self.CurrentShownFrame)
+		self.imWidget.repaint()
+		self.updateCurrentFrameWidgets()
+		
+	def updateCurrentFrameWidgets(self):
+		self.CurrentFrameSlider.blockSignals(True)
+		self.CurrentFrameSlider.setValue(self.CurrentShownFrame)
+		self.CurrentFrameSlider.blockSignals(False)
+		
+		self.CurrentFrameSpinBox.blockSignals(True)
+		self.CurrentFrameSpinBox.setValue(self.CurrentShownFrame)
+		self.CurrentFrameSpinBox.blockSignals(False)
+		
+	def updateFrameWidgetsRange(self):
+		self.CurrentFrameSlider.blockSignals(True)
+		self.CurrentFrameSlider.setRange(0, self.MaxFrames-1)
+		self.CurrentFrameSlider.blockSignals(False)
+		
+		self.CurrentFrameSpinBox.blockSignals(True)
+		self.CurrentFrameSpinBox.setRange(0, self.MaxFrames-1)
+		self.CurrentFrameSpinBox.blockSignals(False)
 	
 	def getSequenceIndexes(self, n):
 		if n > self.MaxFrames:
@@ -80,6 +156,27 @@ class SequenceDisplay(Ui_SequenceDisplayWnd, PyQt4.QtGui.QMainWindow):
 			i = i + 1
 			
 		return (i,n)
+		
+	def playButtonCb(self):
+		if self.IsPlaying == False:
+			self.timer.start(self.PlayInterframe, self)
+			self.IsPlaying = True
+		else:
+			self.timer.stop()
+			self.IsPlaying = False
+			
+	def isLastFrame(self):
+		if self.CurrentShownFrame >= self.MaxFrames -1:
+			return True
+		
+		return False
+		
+	def timerEvent(self, event):
+		if self.IsPlaying == True:
+			self.getNextSequenceFrame()
+			if self.isLastFrame():
+				self.timer.stop()
+				self.IsPlaying = False
 
 if __name__== "__main__":
 	app = PyQt4.QtGui.QApplication(sys.argv)
--- SequenceDisplayGui.py
+++ SequenceDisplayGui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'SequenceDisplayGui.ui'
 #
-# Created: Tue May  7 15:04:27 2013
+# Created: Tue May  7 21:46:50 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
@@ -42,9 +42,30 @@ class Ui_SequenceDisplayWnd(object):
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.CurrentFrameSpinBox = QtGui.QSpinBox(self.centralwidget)
+        self.CurrentFrameSpinBox.setMaximum(100000)
         self.CurrentFrameSpinBox.setObjectName(_fromUtf8("CurrentFrameSpinBox"))
         self.horizontalLayout.addWidget(self.CurrentFrameSpinBox)
+        self.FirstFrameButton = QtGui.QPushButton(self.centralwidget)
+        self.FirstFrameButton.setObjectName(_fromUtf8("FirstFrameButton"))
+        self.horizontalLayout.addWidget(self.FirstFrameButton)
+        self.BackFrameButton = QtGui.QPushButton(self.centralwidget)
+        self.BackFrameButton.setObjectName(_fromUtf8("BackFrameButton"))
+        self.horizontalLayout.addWidget(self.BackFrameButton)
+        self.PlayButton = QtGui.QPushButton(self.centralwidget)
+        self.PlayButton.setObjectName(_fromUtf8("PlayButton"))
+        self.horizontalLayout.addWidget(self.PlayButton)
+        self.ForwardFrameButton = QtGui.QPushButton(self.centralwidget)
+        self.ForwardFrameButton.setObjectName(_fromUtf8("ForwardFrameButton"))
+        self.horizontalLayout.addWidget(self.ForwardFrameButton)
+        self.LastFrameButton = QtGui.QPushButton(self.centralwidget)
+        self.LastFrameButton.setObjectName(_fromUtf8("LastFrameButton"))
+        self.horizontalLayout.addWidget(self.LastFrameButton)
         self.CurrentFrameSlider = QtGui.QSlider(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(7)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.CurrentFrameSlider.sizePolicy().hasHeightForWidth())
+        self.CurrentFrameSlider.setSizePolicy(sizePolicy)
         self.CurrentFrameSlider.setOrientation(QtCore.Qt.Horizontal)
         self.CurrentFrameSlider.setObjectName(_fromUtf8("CurrentFrameSlider"))
         self.horizontalLayout.addWidget(self.CurrentFrameSlider)
@@ -53,7 +74,7 @@ class Ui_SequenceDisplayWnd(object):
         self.verticalLayout.addLayout(self.horizontalLayout)
         SequenceDisplayWnd.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(SequenceDisplayWnd)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 19))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 20))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         SequenceDisplayWnd.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(SequenceDisplayWnd)
@@ -65,4 +86,9 @@ class Ui_SequenceDisplayWnd(object):
 
     def retranslateUi(self, SequenceDisplayWnd):
         SequenceDisplayWnd.setWindowTitle(_translate("SequenceDisplayWnd", "SequenceDisplay", None))
+        self.FirstFrameButton.setText(_translate("SequenceDisplayWnd", "<<", None))
+        self.BackFrameButton.setText(_translate("SequenceDisplayWnd", "<", None))
+        self.PlayButton.setText(_translate("SequenceDisplayWnd", "P", None))
+        self.ForwardFrameButton.setText(_translate("SequenceDisplayWnd", ">", None))
+        self.LastFrameButton.setText(_translate("SequenceDisplayWnd", ">>", None))
 
