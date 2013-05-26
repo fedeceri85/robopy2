8962d7465ae367c93396afdbffbcd1a78deba7a6
Catalin Ciubotaru <catalin.ciubotaru@gmail.com>
2013-05-26 22:05:14 +0200

    some worker upgrades (untested yet)

--- Worker.py
+++ Worker.py
@@ -1,16 +1,32 @@
 import PyQt4
 from PyQt4.QtCore import QThread
-import sys
 
 
 class Worker(QThread):
-	def __init__(self, parent = None, cb=None):
+	def __init__(self, parent = None, cb=None, data=None, withSignals=False):
 		QThread.__init__(self, parent)
 		
 		self.cb = cb
+		self.data = data
+		self.withSignals = withSignals
 		
 	def run(self):
-		if self.cb != None:
-			self.cb()
+		if self.cb != None and self.withSignals=True:
+			if self.data != None:
+				self.cb(self.data)
+			else:
+				self.cb()
+		else:
+			exec()
+			
+			
+	def startJob(self):
 		
+		if self.cb != None:
+			if self.data != None:
+				self.cb(self.data)
+			else:
+				self.cb()
+			
+			self.emit(PyQt4.QtGui.SIGNAL("jobDone()"))
 
